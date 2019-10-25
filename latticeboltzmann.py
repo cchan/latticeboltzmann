e862642010fd2ecc25e86691f0090cb4111766fc
Clive Chan <cc@clive.io>
2019-10-25 01:19:55 -0400

    idk wtf is going on

--- latticeboltzmann.py
+++ latticeboltzmann.py
@@ -5,7 +5,7 @@ import time
 import math
 import cv2
 
-np.seterr(all='raise')
+#np.seterr(all='raise')
 
 w = np.array([1.0/36, 1.0/9, 1.0/36,
               1.0/9, 4.0/9, 1.0/9,
@@ -15,12 +15,15 @@ e = np.array([[-1,-1],[0,-1],[1,-1],
               [-1, 1],[0, 1],[1, 1]])
 e_f = np.asarray(e, dtype=np.float32)
 
-N = 200 # rows
-M = 400 # columns
-OMEGA = 0.4 # affects viscosity
-u_ambient = [0, 0.4] # velocity
+N = 100 # rows
+M = 200 # columns
+OMEGA = 0.8 # affects viscosity
+p_ambient = 10 # density
+u_ambient = [0, 0.3] # velocity
 def isBlocked(y, x):
-  return (x - 50) ** 2 + (y - 50) ** 2 <= 25**2
+  #return x == -10000000
+  #return x == y
+  return (x - 50) ** 2 + (y - 50) ** 2 <= 12**2
 
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
@@ -28,22 +31,19 @@ video = cv2.VideoWriter('./latticeboltzmann.mp4', fourcc, 60, (M, N))
 
 
 # distribution of velocities in a single cell at thermal equilibrium
-def getEquilibrium(velocity):
+def getEquilibrium(velocity, density):
   velocity = np.asarray(velocity, dtype=np.float32)
+  density = np.asarray(density, dtype=np.float32)
   eu = velocity @ e_f.T # relative importance of each available direction
   #print(eu.shape, np.square(eu).shape)
-  try:
-    return w * (1 + 3 * eu + 9/2*np.square(eu) - 3/2*np.expand_dims(np.sum(velocity*velocity,axis=-1),-1))
-  except:
-    print(np.argwhere(velocity>1e14))
-    raise
+  return np.expand_dims(density, -1) * w * (1 + 3 * eu + 9/2*np.square(eu) - 3/2*np.expand_dims(np.sum(velocity*velocity,axis=-1),-1))
 
 # Initialize to a thermally stable continuous flow field, and set the omega values.
 # Note that isBlocked = True means that omega is 1, so no thermal perturbation occurs before reflection back into the rest of the system.
 
 # N rows. M cells in each row.
 
-surroundings = getEquilibrium([u_ambient])[0]
+surroundings = getEquilibrium([u_ambient], [p_ambient])[0]
 print(surroundings)
 print("???", u_ambient, surroundings @ e_f)
 blocked = np.fromfunction(isBlocked, (N, M))
@@ -52,24 +52,27 @@ omega = np.where(np.expand_dims(blocked,-1), np.array(1,ndmin=3), np.array(OMEGA
 
 #sys.stdout.write("\033[2J")
 
-for iter in range(500):
+for iter in range(1000):
   print(iter)
 
   # Collisions (decay toward boltzmann distribution)
-  u = cells @ e_f # net velocity in this cell
   p = np.sum(cells, axis=2) # total density in this cell
+  u = cells @ e_f / np.expand_dims(p, -1) # net velocity in this cell
+  np.nan_to_num(u, copy=False)
   #print(u_ambient, u)
-  equilibrium = np.expand_dims(p, -1) * getEquilibrium(u) # get thermal equilibrium distribution
+  equilibrium = getEquilibrium(u, p) # get thermal equilibrium distribution
   #print(surroundings[0], cells[:,:,0], equilibrium[:,:,0], "!")
   #break
   cells = (1-omega) * equilibrium + omega * cells # decay toward thermal equilibrium
 
   # Display density
-  r = cv2.normalize(u[...,0]* -p, None, 255, 0, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-  g = cv2.normalize(np.zeros(p.shape), None, 255, 0, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-  b = cv2.normalize(u[...,1]* -p, None, 255, 0, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+  gray = np.asarray(255-p * 500, dtype=np.uint8)
+  #video.write(cv2.merge([gray, gray, gray]))
+  r = gray+np.asarray(u[...,0]*500, dtype=np.uint8)
+  g = gray+np.zeros(p.shape, dtype=np.uint8)
+  b = gray+np.asarray(u[...,1]*500, dtype=np.uint8)
   video.write(cv2.merge([r, g, b]))
-
+  
   # Streaming (movement)
   # This step is slow because np.pad copies the array. Can we do it in place?
   for k, (dx, dy) in enumerate(e):
@@ -83,7 +86,8 @@ for iter in range(500):
       cells[...,k] = np.pad(cells[-dx:,-dy:,k], ((0, -dx), (0, -dy)), mode='constant', constant_values=(surroundings[k],))
 
   # Set objects
-  #cells = np.where(np.expand_dims(blocked,-1), np.flip(cells, axis=-1), cells)
+  cells = np.where(np.expand_dims(blocked,-1), np.flip(cells, axis=-1), cells)
+  #cells[40:45:,10,:] = np.array([0,0,0,0,0,5,0,0,0]) * 10
 
   """
   # Display density field
