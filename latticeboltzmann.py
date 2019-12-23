699a21b92a7164a90abd528c5b99eded54b6df35
Clive Chan <cc@clive.io>
2019-12-23 01:46:07 -0500

    separate into functions, all in-place, possibly meaningful results

--- latticeboltzmann.py
+++ latticeboltzmann.py
@@ -33,21 +33,18 @@ assert((np.sum(e, axis=0) == [0, 0]).all())
 e_f = np.asarray(e, dtype=dtype)
 
 # N rows. M cells in each row.
-N = 100 # rows
-M = 200 # columns
-OMEGA = 0.2 # affects viscosity
+N = 400 # rows
+M = 800 # columns
+OMEGA = 0.01 # affects viscosity (0 is completely viscous, 1 is zero viscosity)
 p_ambient = 100 # density
-u_ambient = [0, 0.05] # velocity BUG: why is anything moving when velocity is [0, 0]? (or is it just near 0 and numerically unstable)
+u_ambient = [0, 0.3] # velocity BUG: why is anything moving when velocity is [0, 0]? (or is it just near 0 and numerically unstable)
 def isBlocked(y, x):
-  #return x == -10000000
-  #return x == y
-  return (x - N/2) ** 2 + (y - N/2) ** 2 <= (N/16)**2
+  return np.logical_and(True, (x - N/2) ** 2 + (y - N/2) ** 2 <= (N/16)**2)
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 video = cv2.VideoWriter('./latticeboltzmann.mp4', fourcc, 60, (M, N))
 
 # distribution of velocities in a single cell at thermal equilibrium
-#@profile
 def getEquilibrium(velocity, density):
   eu = velocity @ e_f.T # relative importance of each available direction, by dot product
   #print(eu.shape, np.square(eu).shape)
@@ -59,24 +56,18 @@ surroundings = getEquilibrium(np.array([u_ambient], dtype=dtype), np.array([p_am
 assert(np.isclose(sum(surroundings), p_ambient)) # Conservation of mass
 assert(np.isclose(surroundings @ e_f / p_ambient, u_ambient).all()) # Conservation of momentum
 blocked = np.fromfunction(isBlocked, (N, M))
-cells = np.where(np.expand_dims(blocked,-1), np.array(0,ndmin=3), np.array(surroundings, ndmin=3)) # cells should have k as its first dimension for cache efficiency
 omega = np.where(np.expand_dims(blocked,-1), np.array(1,ndmin=3), np.array(OMEGA, ndmin=3))
 
-#@profile
-def loop(cells):
-  # Collisions (decay toward boltzmann distribution)
-  p = np.sum(cells, axis=2) # total density in this cell
-  with np.errstate(divide='ignore', invalid='ignore'):
-    u = cells @ e_f / np.expand_dims(p, -1) # net velocity in this cell
-  np.nan_to_num(u, copy=False) # Is this a bad hack? if p == 0 (i.e. blocked) then we want u to be zero.
-                               # BUG: actually, the default state should be the one where each outward arrow is in thermal equilibrium
-  #print(u_ambient, u)
-  equilibrium = getEquilibrium(u, p) # get thermal equilibrium distribution
-  #print(surroundings[0], cells[:,:,0], equilibrium[:,:,0], "!")
-  #break
-  cells = equilibrium + omega * (cells - equilibrium) # decay toward thermal equilibrium
 
-  # Display density
+def collide(cells, u, p):
+  equilibrium = getEquilibrium(u, p) # get thermal equilibrium distribution given the net density/velocity in this cell
+
+  # decay toward thermal equilibrium
+  cells -= equilibrium
+  cells *= omega
+  cells += equilibrium
+
+def display(u, p, video):
   #gray = np.asarray(255-p * 500, dtype=np.uint8)
   #video.write(cv2.merge([gray, gray, gray]))
   gray = cv2.normalize(p, None, 0, 255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
@@ -91,24 +82,41 @@ def loop(cells):
   video.write(cv2.cvtColor(cv2.merge([h,s,v]), cv2.COLOR_HSV2BGR))
   #video.write(cv2.merge([r,g,b]))
 
-  # Streaming (movement)
+def stream(cells):
   for k, (dy, dx) in enumerate(e):
     # TODO: Cache locality: move k to be the first dimension.
     cells[max(dy,0):N+min(dy,0), max(dx,0):M+min(dx,0), k] = cells[max(-dy,0):N+min(-dy,0), max(-dx,0):M+min(-dx,0), k]
     cells[:, min(dx,0):max(dx,0),k] = surroundings[k]
     cells[min(dy,0):max(dy,0), :,k] = surroundings[k]
 
-  # Set objects
-  cells = np.where(np.expand_dims(blocked,-1), np.flip(cells, axis=-1), cells)
+def reflect(cells):
+  cells[blocked] = np.flip(cells[blocked], axis=-1)
   #cells[40:45,10,:] = getEquilibrium(np.array([u_ambient])*5, [p_ambient])[0]
 
-  return cells
 
+cells = np.where(np.expand_dims(blocked,-1), np.array(0,ndmin=3), np.array(surroundings, ndmin=3)) # cells should have k as its first dimension for cache efficiency
+stream(cells)
+reflect(cells)
+cells = np.where(np.expand_dims(blocked,-1), cells, np.array(surroundings, ndmin=3))
 
-for iter in range(5000):
+for iter in range(500):
   sys.stdout.write(str(iter)+' ')
   sys.stdout.flush()
-  cells = loop(cells)
+
+  # Get the total density and net velocity for each cell
+  p = np.sum(cells, axis=2) # total density in this cell
+  with np.errstate(divide='ignore', invalid='ignore'):
+    u = cells @ e_f / np.expand_dims(p, -1) # net velocity in this cell
+  np.nan_to_num(u, copy=False) # Is this a bad hack? if p == 0 (i.e. blocked) then we want u to be zero.
+
+  # Collisions (decay toward boltzmann distribution)
+  collide(cells, u, p)
+  # Display density
+  display(u, p, video)
+  # Streaming (movement)
+  stream(cells)
+  # Reflect at object edges
+  reflect(cells)
 
 print()
 
